c008b1f934e40bd39c40ce6539c42de7b7dc8a78
langdonf <lfroker@gmail.com>
2018-11-12 18:29:09 -0800

    added more products and media queries

--- main.js
+++ main.js
@@ -85,21 +85,77 @@ var candyItems = [
 	}
 ];
 
+// Dairy items
+var dairyItems = [
+	{
+		img: './images/dairy/Milk.jpg',
+    name: 'Milk',
+    price: 19.55
+	},
+	{
+		img: './images/dairy/Eggs.jpg',
+    name: 'Eggs',
+    price: 16.25
+	},
+	{
+		img: './images/dairy/Butter.jpg',
+    name: 'Butter',
+    price: 17.95
+	},
+	{
+		img: './images/dairy/Cream.jpg',
+    name: 'Cream',
+    price: 13.65
+	},
+	{
+		img: './images/dairy/Half-Half.jpg',
+    name: 'Half-Half',
+    price: 29.75
+	}
+];
+
+// Meat items
+var meatItems = [
+	{
+		img: './images/meat/bacon.jpg',
+    name: 'Bacon',
+    price: 19.55
+	},
+	{
+		img: './images/meat/ribeye.jpg',
+    name: 'Ribeye',
+    price: 16.25
+	},
+	{
+		img: './images/meat/hot-dogs.jpg',
+    name: 'Hot Dogs',
+    price: 17.95
+	},
+	{
+		img: './images/meat/sausage.jpg',
+    name: 'Sausage',
+    price: 13.65
+	},
+	{
+		img: './images/meat/ground-beef.jpg',
+    name: 'Ground Beef',
+    price: 29.75
+	}
+];
+
 var total = 0;
 
 ////////////////////Adds items to cart///////////////////////////
 addToCart = function(shoppingItem, price){
-  var item = `${shoppingItem} .............. ${price}`;
   var list = document.createElement('li');
+  var item = `${shoppingItem} ..................... ${price}`;
   list.className = "shopping-item";
   list.innerHTML = item;
   document.querySelector('#cart ul').appendChild(list);
   /* Gets price, adds to current total, and appends to cart*/
   total = total + parseFloat(price);
-  
   /* fixes weird bug where total would have like 20 decimal places */
   total = Math.round(100*total)/100;
-
   var totalSum = `Total = $${total}`;
   var totalItem = document.createElement('P');
   totalItem.className = "sum";
@@ -109,7 +165,7 @@ addToCart = function(shoppingItem, price){
 }
 
 
-///////////////Clear Cart///////////////////////
+////////////////////////Clear Cart///////////////////////////
 
 clearCart = function(){
   document.getElementById('totalArea').innerHTML = '';
@@ -118,7 +174,7 @@ clearCart = function(){
 
 document.getElementById('clear').addEventListener('click', clearCart);
 
-/////////////Populate Shopping Area/////////////////
+//////////////////Populate Shopping Area////////////////////
 
 populateCandy = function(){
   document.getElementById('items').innerHTML = '';
@@ -168,6 +224,38 @@ populateJuice = function(){
   };
 }
 
+populateMeat = function(){
+  document.getElementById('items').innerHTML = '';
+  for (let i = 0; i < meatItems.length; i++){
+    var card = `<div class="card"><a href="#">
+                <img src=${meatItems[i].img} alt="" style="width:100%">
+                  <div class="container">
+                    <h4>${meatItems[i].name}</h4>
+                    <h5>${meatItems[i].price}</h5>
+                  </div></a>
+                </div>`;
+    var div = document.createElement('P');
+    div.innerHTML = card;
+    document.getElementById('items').appendChild(div);
+  };
+}
+
+populateDairy = function(){
+  document.getElementById('items').innerHTML = '';
+  for (let i = 0; i < dairyItems.length; i++){
+    var card = `<div class="card"><a href="#">
+                <img src=${dairyItems[i].img} alt="" style="width:100%">
+                  <div class="container">
+                    <h4>${dairyItems[i].name}</h4>
+                    <h5>${dairyItems[i].price}</h5>
+                  </div></a>
+                </div>`;
+    var div = document.createElement('P');
+    div.innerHTML = card;
+    document.getElementById('items').appendChild(div);
+  };
+}
+
 ///////////Grabs Product Information//////////////
 $('#items').on('click', '.card', function() {
   var itemName = $(this).find('h4').text();
@@ -177,9 +265,8 @@ $('#items').on('click', '.card', function() {
 );
 
 //////////////////Adds Event Listeners///////////
-
 document.getElementById('juice').addEventListener('click', populateJuice);
-
 document.getElementById('cereal').addEventListener('click', populateCereal);
-
 document.getElementById('candy').addEventListener('click', populateCandy);
+document.getElementById('dairy').addEventListener('click', populateDairy);
+document.getElementById('meat').addEventListener('click', populateMeat);
